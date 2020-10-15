6e513105f753c7f2208c55bac5c5a8630bf0523e
kovacspe <pet.kovacs1@gmail.com>
2020-10-15 20:02:21 +0200

    baseline model implemented

--- models.py
+++ models.py
@@ -1,9 +1,15 @@
+import numpy as np
+import datetime as dt
+
+
 class BaselineModel:
     def __init__(self, data, model_params):
         self.data = data
-
-    def fit(self):
         pass
 
-    def predict(self):
-        return data
+    def fit(self, start, end):
+        pass
+
+    def predict(self, start, end):
+        indicies = self.data.get_subset(start, end).index
+        return self.data.data.loc[min(indicies)-1:max(indicies)-1, 'Volume']
